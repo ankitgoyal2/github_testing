60f18c903ede51d617fb625bc5e3cc9aa70b9ed4
ankit <goyala960@gmail.com>
2019-09-23 19:20:54 +0530

    initial commit

--- app/src/main/java/com/example/github_testing/MainActivity.java
+++ app/src/main/java/com/example/github_testing/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    int x;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
