f687b7b94751a1a2de621f9b87088a3c35f2a12a
ankit <goyala960@gmail.com>
2019-09-24 17:59:28 +0530

    hfkjdshfksd

--- app/src/main/java/com/example/github_testing/MainActivity.java
+++ app/src/main/java/com/example/github_testing/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     int z;
     int a;
 
-
+    int c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
